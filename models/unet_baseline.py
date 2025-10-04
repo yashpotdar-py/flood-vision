5158b322d9949b11596509077c3645788a8de322
apollo <apollo.yashyogeshpotdar7@gmail.com>
2025-10-04 23:51:08 +0530

    feat(model): :sparkles: add U-Net baseline model with ResNet34 encoder

---
+++ models/unet_baseline.py
@@ -0,0 +1,32 @@
+"""
+models/unet_baseline.py
+
+UNet model definition using segmentation_models_pytorch library.
+Usage:
+    pip install segmentation-models-pytorch
+    from models.unet_baseline import get_unet_model
+    model = get_unet_model(num_classes=10, encoder="resnet34",
+                            pretrained=True)
+    output = model(input_tensor)  # input_tensor shape: (B, 3, H, W)
+"""
+
+import segmentation_models_pytorch as smp
+
+def get_unet_model(num_classes=10, encoder="resnet34",
+                   pretrained=True):
+    """
+    Returns a UNet model from segmentation_models_pytorch with specified encoder and number of classes.
+    Args:
+        num_classes (int): Number of output segmentation classes.
+        encoder (str): Backbone encoder architecture.
+        pretrained (bool): Use ImageNet pre-trained
+    Returns: 
+        torch.nn.Module: UNet model
+    """
+    model = smp.Unet(
+        encoder_name=encoder,
+        encoder_weights="imagenet" if pretrained else None,
+        in_channels=3,
+        classes=num_classes,
+    )
+    return model
